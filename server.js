bfda60ff5487193f22c2d9e70a62db9846dd606e
karlos-silva <karlossilva194@gmail.com>
2020-04-29 11:39:30 -0300

    fixing bugs in the css

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 const express = require('express')
-const nunjucks = require('nunjucks')
 
 const server = express()
 
-server.use(express.static('src'))
-
-server.set("view engine", "html")
-
-nunjucks.configure("views", {
-  express: server
-})
-
 server.get("/", function(req, res){
-  return res.render("index")
+  res.sendFile(__dirname+"/views/index.html")
 })
 
 server.get("/home", function(req, res){
-  return res.render("home")
+  res.sendFile(__dirname+"/views/home.html")
 })
 
 server.listen(8080, function() {
