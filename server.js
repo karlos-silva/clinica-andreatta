e8ad71e042d0b97e98a376a070bf18f0879c183e
karlos-silva <karlossilva194@gmail.com>
2020-05-04 20:36:08 -0300

    fixing bug

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const server = express()
 
+server.use(express.static("public"));
+
 server.get("/", function(req, res){
   res.sendFile(__dirname+"/views/index.html")
 })
