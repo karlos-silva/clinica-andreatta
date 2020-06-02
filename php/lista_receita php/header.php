36d3a5be8ef7ba3ac8326e27696057b38e5b1f77
Bernardo Giraldi Andreatta <55238677+Bernardo-Andreatta@users.noreply.github.com>
2020-06-02 00:15:09 -0300

    Lista de receitas

---
+++ php/lista_receita php/header.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="../../css/agend_style.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,600&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="../../img/logo.ico" />
+    <title>Clinica Andreatta</title>
+  </head>
+  <body>
+<nav class="navbar navbar-expand-lg navbar-light">
+
+    <a class="navbar-brand brand"  href="#"><img src="../../img/logo.png" alt="" > Andreatta</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="../home_cliente php/home.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../consulta php/index.php">Marcar Consulta</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../agenda_cliente/index.php">Consultas Marcadas</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../lista_receita php/index.php">Receitas medicas</a>
+        </li>
+      </ul>
+    </div>
+    <a href="logout.php" ><i class="fas fa-sign-out-alt sair right"></i></a>
+</nav>
